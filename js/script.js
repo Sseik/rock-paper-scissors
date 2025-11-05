c9c2de40576cad9b29416105c28af01b9bf96f0d
Dmytro Hapchyn <48380610+Sseik@users.noreply.github.com>
2025-11-05 11:19:37 +0200

    Add variables for saving scores

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 // Not using arrays because they are yet to be covered in the lessons
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
